d55e65e22fef69bd3f88c90629a1076550b04c99
Matthias <12-build@web.de>
2022-04-24 21:10:27 +0200

    added leddet as remote resource + added resource fields in class

--- labgrid/resource/led.py
+++ labgrid/resource/led.py
@@ -5,10 +5,13 @@ import attr
 from .common import ManagedResource, ResourceManager
 from ..factory import target_factory
 
+
 @attr.s(eq=False)
 class LedDetection(ResourceManager):
-    _clients = attr.ib(default=attr.Factory(dict), validator=attr.validators.instance_of(dict))
-    
+    host = attr.ib(validator=attr.validators.instance_of(str))
+    mqtt_port = attr.ib(validator=attr.validators.instance_of(str))
+    reference = attr.ib(validator=attr.validators.instance_of(str))
+    webcam_id = attr.ib(validator=attr.validators.instance_of(str))
+
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
-    
--- labgrid/resource/remote.py
+++ labgrid/resource/remote.py
@@ -314,3 +314,12 @@ class NetworkMQTTResource(ManagedResource):
 
     host = attr.ib(validator=attr.validators.instance_of(str))
     avail_topic = attr.ib(validator=attr.validators.instance_of(str))
+
+
+@attr.s(eq=False)
+class NetworkLedDetection(ManagedResource):
+    manager_cls = RemotePlaceManager
+
+    host = attr.ib(validator=attr.validators.instance_of(str))
+    avail_topic = attr.ib(validator=attr.validators.instance_of(str))
+
